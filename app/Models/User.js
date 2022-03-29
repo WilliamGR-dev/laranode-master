4803088763ff29b1d8190cf31a9941ed5019cf39
WilliamGR-dev <wgirardreydet@gmail.com>
2022-03-29 21:43:33 +0200

    patch User MYSQL return

--- app/Models/User.js
+++ app/Models/User.js
@@ -7,17 +7,17 @@ function findAll() {
 }
 
 function findById(id) {
-    let request = db.client.query(`SELECT * FROM users WHERE id = $1`, [id])
+    let request = db.client.query(`SELECT id, name, email, created_at, updated_at FROM users WHERE id = $1`, [id])
     return request;
 }
 
 function create(name, email, password) {
-    let request = db.client.query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING (name, email, created_at, updated_at)" , [name, email, password])
+    let request = db.client.query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email, created_at, updated_at" , [name, email, password])
     return request;
 }
 
 function update(id, name, email, password) {
-    let request = db.client.query(`UPDATE users SET name = $1, email = $2, password = $3 WHERE id = ${id} RETURNING (name, email, created_at, updated_at)`, [name, email, password])
+    let request = db.client.query(`UPDATE users SET name = $1, email = $2, password = $3 WHERE id = ${id} RETURNING id, name, email, created_at, updated_at`, [name, email, password])
     return request;
 }
 
